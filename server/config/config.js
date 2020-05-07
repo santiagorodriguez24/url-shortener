20eee6e40780880a8b4453e6d2481b3325ef0feb
Santiago <santiagomanuelrodriguez@gmail.com>
2020-05-07 17:50:18 -0300

    readme added

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 process.env.PORT = process.env.PORT || 3001;
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'developmentchuchi';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 let urlDB;
 
-if (process.env.NODE_ENV === 'developmentchuchi') {
+if (process.env.NODE_ENV === 'development') {
     urlDB = 'mongodb://localhost:27017/url-shortener';
 } else {
     urlDB = process.env.mongo_database_url;
